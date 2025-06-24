c4b78dd38ad933db7d3b9e97105987a9cdffcc61
Leandro Peres <leandroperes@protonmail.com>
2025-06-24 11:18:48 -0300

    feat: VueUse

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+    ],
+  },
   plugins: [UnoCSS(), Vue({
     include: [/\.vue$/, /\.md$/],
   })],
