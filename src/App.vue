8b9a4294f8734d186185766066a4b24e85f93509
Leandro Peres <leandroperes@protonmail.com>
2025-06-24 16:50:39 -0300

    feat: Router blur transition

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
-const route = useRoute()
+import Footer from './components/Footer.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
+  <!-- Main navigation bar -->
   <NavBar />
+
+  <!-- Main content -->
   <main class="px-7 py-10 of-x-hidden">
-    <RouterView />
-    <Footer :key="route.path" />
+    <!-- Router -->
+    <RouterView v-slot="{ Component, route }">
+      <!-- Transition -->
+      <Transition :name="route.meta?.transition || 'blur'" mode="out-in" appear>
+        <!-- Page -->
+        <div :key="route.path" class="page-wrapper">
+          <component :is="Component" />
+
+          <!-- Footer -->
+          <Footer />
+        </div>
+      </Transition>
+    </RouterView>
   </main>
 </template>
+
+<style lang="css" scoped>
+.page-wrapper {
+  width: 100%;
+}
+
+/* Blur transition */
+.blur-enter-active,
+.blur-leave-active {
+  transition: all 0.2s ease;
+}
+
+.blur-enter-from,
+.blur-leave-to {
+  filter: blur(0.5rem);
+  opacity: 0;
+}
+</style>
