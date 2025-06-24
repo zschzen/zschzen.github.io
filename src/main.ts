26f591782d55ee983fe10b828d99bda6e28a6191
Leandro Peres <leandroperes@protonmail.com>
2025-06-24 17:00:05 -0300

    feat: `NProgress`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import NProgress from 'nprogress'
+import { createPinia } from 'pinia'
 import { ViteSSG } from 'vite-ssg'
+
 import { routes } from 'vue-router/auto-routes'
 
 import App from './App.vue'
-
 import '@unocss/reset/tailwind.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
 import 'markdown-it-github-alerts/styles/github-colors-light.css'
@@ -15,4 +17,16 @@ import 'uno.css'
 export const createApp = ViteSSG(
   App,
   { routes },
+  ({ router, app, isClient }) => {
+    app.use(createPinia())
+
+    if (isClient) {
+      router.beforeEach(() => {
+        NProgress.start()
+      })
+      router.afterEach(() => {
+        NProgress.done()
+      })
+    }
+  },
 )
