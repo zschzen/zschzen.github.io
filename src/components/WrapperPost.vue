86d4bc12c44b0f5ef44d0b750269375f2a986e3d
Leandro Peres <leandroperes@protonmail.com>
2025-06-26 17:48:29 -0300

    feat: Implement formatDate
    + a fix onto `cd ..`

--- src/components/WrapperPost.vue
+++ src/components/WrapperPost.vue
@@ -1,4 +1,6 @@
 <script setup lang='ts'>
+import { formatDate } from '~/logics/utils'
+
 const { frontmatter } = defineProps({
   frontmatter: {
     type: Object,
@@ -21,6 +23,12 @@ onMounted(() => {
     <h1 class="mb-0 slide-enter-50">
       {{ frontmatter.display ?? frontmatter.title }}
     </h1>
+    <p
+      v-if="frontmatter.date"
+      class="opacity-50 !-mt-6 slide-enter-50"
+    >
+      {{ formatDate(frontmatter.date, false) }} <span v-if="frontmatter.duration">· {{ frontmatter.duration }}</span>
+    </p>
     <p v-if="frontmatter.place" class="mt--4!">
       <span op50>at </span>
       <a v-if="frontmatter.placeLink" :href="frontmatter.placeLink" target="_blank">
@@ -46,7 +54,7 @@ onMounted(() => {
   <div v-if="route.path !== '/'" class="prose m-auto mt-8 mb-8 slide-enter animate-delay-500 print:hidden">
     <span font-mono op50>> </span>
     <RouterLink
-      to=".."
+      :to="route.path.split('/').slice(0, -1).join('/') || '/'"
       class="font-mono op50 hover:op75"
       v-text="'cd ..'"
     />
