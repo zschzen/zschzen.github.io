d3aed980b4ffe925566f30b7927d16a11432276d
Leandro Peres <leandroperes@protonmail.com>
2025-06-26 08:18:20 -0300

    chore: Adjust logo size

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,5 +3,5 @@ import LogoSVG from '../../public/logo.svg?component'
 </script>
 
 <template>
-  <LogoSVG h-20 w-20 class="fill-[#161616] dark:fill-[#F2F4F8]" />
+  <LogoSVG h-16 w-16 class="fill-[#161616] dark:fill-[#F2F4F8]" />
 </template>
