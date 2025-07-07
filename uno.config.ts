80d313c0fc513ca948f8a6c45b9a221a4668bd2e
Leandro Peres <leandroperes@protonmail.com>
2025-07-07 18:15:26 -0300

    feat: Base colors shortcut

--- uno.config.ts
+++ uno.config.ts
@@ -5,6 +5,11 @@ import { defineConfig, presetAttributify, presetIcons, presetWebFonts, presetWin
 
 export default defineConfig({
   shortcuts: [
+    {
+      'bg-base': 'bg-[#F2F4F8] dark:bg-[#161616]',
+      'color-base': 'text-black dark:text-white',
+      'border-base': 'border-[#8884]',
+    },
     [/^btn-(\w+|\[#[0-9a-fA-F]{6}(?:[0-9a-fA-F]{2})?\])$/, ([_, color]) => `op50 px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
   ],
   rules: [
