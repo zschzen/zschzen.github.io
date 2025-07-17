669e37737c0ec02d3f49d32141d2dce923889c3d
Leandro Peres <leandroperes@protonmail.com>
2025-07-17 10:13:41 -0300

    chore(btn): Remove opacity

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       'color-base': 'text-black dark:text-white',
       'border-base': 'border-[#8884]',
     },
-    [/^btn-(\w+|\[#[0-9a-fA-F]{6}(?:[0-9a-fA-F]{2})?\])$/, ([_, color]) => `op50 px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
+    [/^btn-(\w+|\[#[0-9a-fA-F]{6}(?:[0-9a-fA-F]{2})?\])$/, ([_, color]) => `px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
   ],
   rules: [
     [/^slide-enter-(\d+)$/, ([_, n]) => ({
